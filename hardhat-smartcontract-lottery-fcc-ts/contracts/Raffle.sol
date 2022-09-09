905b12f503cb74ed9edb2a0013c49e905e951255
simutis <54304022+SimasG@users.noreply.github.com>
2022-09-09 11:02:52 +0200

    completed basic raffle contract functionality

--- hardhat-smartcontract-lottery-fcc-ts/contracts/Raffle.sol
+++ hardhat-smartcontract-lottery-fcc-ts/contracts/Raffle.sol
@@ -1,6 +1,6 @@
 // * Things we want to do:
 // * 1. Enabling user to enter the lottery (by paying some amount) -> DONE
-// * 2. Pick a (verifiably) random winner -> WIP
+// * 2. Pick a (verifiably) random winner -> DONE
 // * 3. Winner to be selected every X minutes -> completely automated
 // * We'll be relying on Chainlink a lot. Randomness (verifiable random function (VRF)), Automated Execution (Chainlink Keepers)
 
@@ -10,13 +10,28 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol"; // This contract holds "fulfillRandomWords" func
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+// Ensures we implement "checkUpkeep()" & "performUpkeep()". Does it enable us running these funcs as well? I think so
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 
 error Raffle__NotEnoughETHEntered();
 error Raffle__TransferFailed();
+error Raffle__NotOpen();
+error Raffle__UpkeepNotNeeded(uint256 currentBalance, uint256 numPlayers, uint256 raffleState);
 
 // "Raffle" contract is inheriting the functionality of "VRFConsumerBaseV2" contract ()
 // In JS it would be "contract Raffle extends VRFConsumerBaseV2"
-contract Raffle is VRFConsumerBaseV2 {
+/**@title A sample Raffle Contract
+ * @author Patrick Collins
+ * @notice This contract is for creating a sample raffle contract
+ * @dev This implements the Chainlink VRF Version 2 & Chainlink Keepers
+ */
+contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
+    /* Type Declarations */
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+
     /* State Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players; // Making the addresses payable so we could pay the winner
@@ -26,23 +41,28 @@ contract Raffle is VRFConsumerBaseV2 {
     uint32 private immutable i_callbackGasLimit;
     // ** Could we decrease the var size even more (why would a constant value of 3 take 16 bits (2^16) of storage)?
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
-    uint16 private constant NUM_WORDS = 1;
+    uint32 private constant NUM_WORDS = 1;
 
     // Lottery Variables
     address private s_recentWinner;
+    RaffleState private s_raffleState;
+    uint256 private s_lastTimeStamp;
+    uint256 private immutable i_interval;
 
     /* Events */
     event RaffleEnter(address indexed player);
     event ReqestedRaffleWinner(uint256 indexed requestId);
     event WinnerPicked(address indexed winner);
 
+    /* Functions */
     // "vrfCoordinatorV2" is an address of the contract that will do the random verification
     constructor(
-        address vrfCoordinatorV2,
+        address vrfCoordinatorV2, // contract address
         uint256 entranceFee,
-        bytes32 gasLane,
+        bytes32 gasLane, // keyHash
         uint64 subscriptionId,
-        uint32 callbackGasLimit
+        uint32 callbackGasLimit,
+        uint256 interval
     ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
         // ** Is "vrfCoordinator" a contract or an interface? Patrick said contract
@@ -51,22 +71,64 @@ contract Raffle is VRFConsumerBaseV2 {
         i_gasLane = gasLane;
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
+        s_raffleState = RaffleState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        i_interval = interval;
     }
 
     // "public" so anyone can enter our raffle | "payable" so anyone could send funds to the contract
     function enterRaffle() public payable {
         // Storing only the error code instead of the error msg string -> more gas efficient
         if (msg.value < i_entranceFee) revert Raffle__NotEnoughETHEntered();
+        if (s_raffleState != RaffleState.OPEN) revert Raffle__NotOpen();
         s_players.push(payable(msg.sender)); // typecasting "msg.sender" to be a payable address
 
         // Emit an event when we update a dynamic array or mapping
         emit RaffleEnter(msg.sender);
     }
 
+    /**
+     * @dev This is the func that Chainlink Keeper nodes call to look for the "upkeepNeeded" to return
+     * "true". Once it's true, "performUpkeep()" func is triggered
+     * To be true, the following should be true:
+     * 1. Our specified time internal should have passed
+     * 2. The lottery should have at least 1 player + have some ETH
+     * 3. Our subscription is funded with LINK
+     * 4. The lottery should be in an "open" state
+     */
+    function checkUpkeep(
+        bytes memory /* checkData */
+    )
+        public
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
+        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval); // Condition 1
+        bool hasPlayers = s_players.length > 0; // Condition 2
+        bool hasBalance = address(this).balance > 0; // Codition 3
+        bool isOpen = RaffleState.OPEN == s_raffleState; // Condition 4
+        upkeepNeeded = (timePassed && hasPlayers && hasBalance && isOpen);
+    }
+
     // This func will be called by the Chainlink Keepers network -> aka it can automatically run without
     // us needing to interact with it
     // Transaction 1 in generating randomness (request to Chainlink)
-    function requestRandomWinner() external {
+    // "requestRandomWinner()" -> "performUpkeep()"
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external override {
+        (bool upkeepNeeded, ) = checkUpkeep(""); // ** Why do strings (e.g. empty string) not work w calldata?
+        if (!upkeepNeeded)
+            revert Raffle__UpkeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(s_raffleState)
+            );
+
+        s_raffleState = RaffleState.CALCULATING;
         uint256 requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane, // called "keyHash" in docs | gasLane -> gas limit/ceiling
             i_subscriptionId,
@@ -86,6 +148,9 @@ contract Raffle is VRFConsumerBaseV2 {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
         s_recentWinner = recentWinner;
+        s_raffleState = RaffleState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
 
         // Sending winnings to winner
         // "address(this)" -> the address of this contract "Raffle"
@@ -107,5 +172,27 @@ contract Raffle is VRFConsumerBaseV2 {
 
     function getRecentWinner() public view returns (address) {
         return s_recentWinner;
+    }
+
+    function getRaffleState() public view returns (RaffleState) {
+        return s_raffleState;
+    }
+
+    // "getNumWords()" can be "pure" cause we don't even need to read the blockchain to access the value
+    // ** Shouldn't it return a "uint32", just like in its declaration?
+    function getNumWords() public pure returns (uint32) {
+        return NUM_WORDS;
+    }
+
+    function getNumberOfPlayers() public view returns (uint256) {
+        return s_players.length;
+    }
+
+    function getlatestTimeStamp() public view returns (uint256) {
+        return s_lastTimeStamp;
+    }
+
+    function getRequestConfirmations() public pure returns (uint16) {
+        return REQUEST_CONFIRMATIONS;
     }
 }
---
+++ hardhat-smartcontract-lottery-fcc-ts/deploy/01-deploy-raffle.ts
@@ -0,0 +1,7 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+async function deployRaffle(hre: HardhatRuntimeEnvironment) {
+  const { getNamedAccounts, deployments } = hre;
+}
+
+export default deployRaffle;
